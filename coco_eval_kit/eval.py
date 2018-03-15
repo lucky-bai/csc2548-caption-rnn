6aa48594f22f828e8546be460c43f020ad325311
Bai Li <bai@cs.toronto.edu>
2018-03-15 16:30:14 -0400

    Eval script runner

---
+++ coco_eval_kit/eval.py
@@ -0,0 +1,32 @@
+from pycocotools.coco import COCO
+from pycocoevalcap.eval import COCOEvalCap
+import matplotlib.pyplot as plt
+
+import json
+from json import encoder
+encoder.FLOAT_REPR = lambda o: format(o, '.3f')
+
+
+# set up file names and pathes
+dataDir='..'
+dataType='val2014'
+algName = 'fakecap'
+annFile='%s/annotations/captions_%s.json'%(dataDir,dataType)
+subtypes=['results', 'evalImgs', 'eval']
+[resFile, evalImgsFile, evalFile]= ['%s/results/captions_%s_%s_%s.json'%(dataDir,dataType,algName,subtype) for subtype in subtypes]
+resFile = 'valid.json'
+
+# create coco object and cocoRes object
+coco = COCO(annFile)
+cocoRes = coco.loadRes(resFile)
+
+# create cocoEval object by taking coco and cocoRes
+cocoEval = COCOEvalCap(coco, cocoRes)
+
+# evaluate on a subset of images by setting
+# cocoEval.params['image_id'] = cocoRes.getImgIds()
+# please remove this line when evaluating the full validation set
+cocoEval.params['image_id'] = cocoRes.getImgIds()
+
+# evaluate results
+cocoEval.evaluate()
